caa72a6edab21948a2cf8ad3771de0e384125cb9
Steve Callan <steve.callan@callaninteractive.com>
2013-10-29 09:18:55 -0400

    Correct langHeader from previous merge conflict

--- js/fb.gallery.js
+++ js/fb.gallery.js
@@ -4,7 +4,7 @@
     
         /* Setup Defaults */
         var settings = $.extend({
-            lang_header: "Browse Your Albums",
+            langHeader: "Browse Your Albums",
             facebookUserID: "",
             onPhotoSelect: function(){},
             onAlbumSelect: function(){}
@@ -93,7 +93,7 @@
         /* Load up the albums */
         var fbgallery_loadAlbums = function(){
             fbgallery_pluginDebug("Loading Albums");
-            $("#jq-fbgallery-container h1").html(settings.lang_header);
+            $("#jq-fbgallery-container h1").html(settings.langHeader);
             
             FB.api('/me/albums', function(response) {
                 $("#jq-fbgallery-results-container").html(""); 
@@ -159,7 +159,7 @@
             fbgallery_pluginDebug("Launching Modal");
             
             /* Create a container */
-            $("body").prepend('<div id="jq-fbgallery-container"><a href="#" id="jq-fbgallery-container-close">&times;</a><h1>' + settings.lang_header + '</h1> <div id="jq-fbgallery-results-container"></div></div><div id="jq-fbgallery-overlay"></div>');
+            $("body").prepend('<div id="jq-fbgallery-container"><a href="#" id="jq-fbgallery-container-close">&times;</a><h1>' + settings.langHeader + '</h1> <div id="jq-fbgallery-results-container"></div></div><div id="jq-fbgallery-overlay"></div>');
             
             /* Set CSS on the above container */
             fbgallery_setCSS();
